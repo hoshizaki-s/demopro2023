0328061c27a8a86eb87352949d6a02fd10d85d98
hoshizaki-s <hoshizaki-s@roboken.iit.tsukuba.ac.jp>
2023-09-25 18:35:56 +0900

    first operatetion v

---
+++ src/harisen_controller.cpp
@@ -0,0 +1,163 @@
+#include <ros/ros.h>
+#include <std_msgs/Int32.h>
+#include "std_msgs/String.h"
+#include "dynamixel_sdk_examples/GetPosition.h"
+#include "dynamixel_sdk_examples/SetPosition.h"
+#include "dynamixel_sdk/dynamixel_sdk.h"
+
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Quaternion.h>
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_broadcaster.h>
+
+#include <map>
+#include <math.h>
+
+#define ARM_CLOSE 800
+#define ARM_OPEN 0
+
+using namespace dynamixel;
+
+bool flag1_ = false;
+bool flag2_ = false;
+bool end_flag_ = false;
+ros::Publisher set_position_pub_;
+ros::Subscriber first_flag_sub_;
+ros::Subscriber second_flag_sub_; 
+
+
+// オドメトリから得られる現在の位置と姿勢
+double robot_x, robot_y;
+double roll, pitch, yaw;
+geometry_msgs::Quaternion robot_r;
+
+geometry_msgs::Twist twist; // 指令する速度、角速度
+
+std::map<std::string, double> params_; // パラメータをここに格納
+geometry_msgs::PoseStamped goal; // 目標地点
+
+// オドメトリのコールバック
+void odom_callback(const nav_msgs::Odometry::ConstPtr &msg)
+{
+	robot_x = msg->pose.pose.position.x;
+	robot_y = msg->pose.pose.position.y;
+	robot_r = msg->pose.pose.orientation;
+}
+
+// クォータニオンをオイラーに変換                                               
+void geometry_quat_to_rpy(double &roll, double &pitch, double &yaw, geometry_msgs::Quaternion geometry_quat)
+{
+    tf::Quaternion quat;
+    quaternionMsgToTF(geometry_quat, quat);
+    tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);
+}
+
+//　goalで指定した位置に近いかの判定を行う
+int near_position(geometry_msgs::PoseStamped goal)
+{
+	double difx = robot_x - goal.pose.position.x;
+	double dify = robot_y - goal.pose.position.y;
+	return (sqrt(difx * difx + dify * dify) < 0.2);
+}
+
+
+void go_position(geometry_msgs::PoseStamped goal)
+{
+    double k_v = 0.1; // 速度の係数	0.1
+    double k_w = 0.1; // 角速度の係数
+	
+	// 指令する速度と角速度
+	double v = 0.0;
+	double w = 0.0;
+
+	//　角速度の計算
+	double theta = atan2(goal.pose.position.y - robot_y, goal.pose.position.x - robot_x);
+	while (theta <= -M_PI || M_PI <= theta)
+	{
+		if (theta <= -M_PI)
+			theta = theta + 2 * M_PI;
+		else
+			theta = theta - 2 * M_PI;
+	}
+
+	// 現在のロボットのroll, pitch, yawを計算
+	geometry_quat_to_rpy(roll, pitch, yaw, robot_r);
+
+	while (yaw <= -M_PI || M_PI <= yaw)
+	{
+		if (yaw <= -M_PI)
+			yaw = yaw + 2 * M_PI;
+		else
+			yaw = yaw - 2 * M_PI;
+	}
+
+	theta = theta - yaw; //thetaに目標とする点に向くために必要となる角度を格納 角度の差を取る処理は関数化したほうが良い。
+
+	while (theta <= -M_PI || M_PI <= theta)
+	{
+		if (theta <= -M_PI)
+			theta = theta + 2 * M_PI;
+		else
+			theta = theta - 2 * M_PI;
+	}
+
+	w = k_w * theta;
+
+	// 速度の計算(追従する点が自分より前か後ろかで計算を変更)	クリッピングも関数化したほうが良い。
+	if (theta <= M_PI / 2 && theta >= -M_PI / 2)
+		v = k_v * ((goal.pose.position.x - robot_x) * (goal.pose.position.x - robot_x) + (goal.pose.position.y - robot_y) * (goal.pose.position.y - robot_y));
+	else
+		v = -k_v * ((goal.pose.position.x - robot_x) * (goal.pose.position.x - robot_x) + (goal.pose.position.y - robot_y) * (goal.pose.position.y - robot_y));
+	
+	// publishする値の格納
+	twist.linear.x = v;
+	twist.linear.y = 0.0;
+	twist.linear.z = 0.0;
+	twist.angular.x = 0.0;
+	twist.angular.y = 0.0;
+	twist.angular.z = w;
+
+	std::cout << "v: " << v << ", w: " << w << std::endl;
+
+}
+
+void check_flag(){
+    if(flag1_ && flag2_){
+        dynamixel_sdk_examples::SetPosition msg;
+        msg.id = 1;
+        msg.position = ARM_OPEN;
+        set_position_pub_.publish(msg);
+        end_flag_ = true;
+    }
+}
+void flag1_callback(const std_msgs::Int32 flag){
+    flag1_ = (0==flag.data);
+    check_flag();
+}
+void flag2_callback(const std_msgs::Int32 flag){
+    flag2_ = (0==flag.data);
+    check_flag();
+}
+
+int main(int argc, char ** argv){
+    ros::init(argc, argv, "harisen_controller");
+    ros::NodeHandle nh_;
+    set_position_pub_ = nh_.advertise<dynamixel_sdk_examples::SetPosition>("/set_position", 10);
+    first_flag_sub_ = nh_.subscribe("/first_flag", 10, flag1_callback);
+    second_flag_sub_ = nh_.subscribe("/second_flag", 10, flag2_callback);
+    
+    dynamixel_sdk_examples::SetPosition msg;
+    msg.id = 1;
+    msg.position = ARM_CLOSE;
+    set_position_pub_.publish(msg);
+    sleep(1);
+    set_position_pub_.publish(msg);
+    
+    while(ros::ok()){
+        if(end_flag_)break;
+        ros::spinOnce();
+    }
+    return 0;
+
+}
